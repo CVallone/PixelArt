327b56aaad29fa9cde5e3a944cce70765a7c2c82
Christopher Vallone <Chris.vallone3@gmail.com>
2022-01-23 17:48:23 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,15 +1,17 @@
+//declare the variables for the color, table, and sizePicker
 const color = document.getElementById('colorPicker');
 const table = document.getElementById('pixelCanvas');
 const sizePicker = document.getElementById('sizePicker');
-
+//declare the variables
 let height = document.getElementById('inputHeight').value;
 let width = document.getElementById('inputWidth').value;
 makeGrid(height, width);
 
+//this code erases the previously saved grid
 sizePicker.addEventListener('click', (e) => {
 
-    e.preventDefault();
-
+    e.preventDefault(); //this prevents the screen from rendering nothing,
+    // if left out, the grid remains blank although the variables may be stored
     let height = document.getElementById('inputHeight').value;
     let width = document.getElementById('inputWidth').value;
     table.firstElementChild.remove();
@@ -17,6 +19,7 @@ sizePicker.addEventListener('click', (e) => {
     makeGrid(height,width);
 });
 
+//this is the function that spans the grid
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
         let row = table.insertRow(i);
